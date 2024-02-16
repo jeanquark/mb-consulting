b323fe809cd6d569f968bda1698705d01e89345a
Jean-Marc <jm.kleger@gmail.com>
2024-02-16 18:41:06 +0100

    Make logo visible on mobile navbar

--- scripts/toggle-dropdown.js
+++ scripts/toggle-dropdown.js
@@ -1,26 +1,20 @@
-/* When the user clicks on the button, toggle between hiding and showing the dropdown content */
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (e) {
-    // console.log('onclick e: ', e)
-    if (!e.target.matches('.dropBtnTarget')) {
-        var myDropdown = document.getElementById('myDropdown')
-        // console.log('myDropdown: ', myDropdown);
-        if (myDropdown.classList.contains('show')) {
-            myDropdown.classList.remove('show')
-        }
-    }
-}
-
 function toggleDropdown() {
     console.log('toggleDropdown')
-    // return
-    console.log('document.getElementById("myDropdown").classList: ', document.getElementById('myDropdown').classList)
     document.getElementById('myDropdown').classList.toggle('show')
 }
 
 function toggleDropdownMobile() {
-    console.log('toggleDropdownMobile')
+    console.log('toggleDropdownMobile2')
     console.log('document.getElementById("dropdownMobile").classList: ', document.getElementById('dropdownMobile').classList)
     document.getElementById('dropdownMobile').classList.toggle('show-dropdown-content-mobile')
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+    if (!e.target.matches('.dropDownTarget')) {
+        var myDropdown = document.getElementById('myDropdown')
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show')
+        }
+    }
 }
